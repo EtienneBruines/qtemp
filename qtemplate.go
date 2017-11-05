82fd3b92072239dabf07b88e2abe0aff0f6a5740
Etienne Bruines <etienne@bruines.com>
2017-11-05 20:25:11 +0100

    Initial release

---
+++ qtemplate.go
@@ -0,0 +1,87 @@
+package qtemplate
+
+import (
+	"html/template"
+	"log"
+	"path/filepath"
+	"strings"
+	"sync"
+
+	"github.com/pkg/errors"
+	"github.com/valyala/fasthttp"
+)
+
+var (
+	TemplateDir = "templates"
+	mgr         = templateManager{templates: make(map[string]*template.Template)}
+)
+
+type templateManager struct {
+	templates map[string]*template.Template
+	master    *template.Template
+	mu        sync.RWMutex
+}
+
+func (m *templateManager) Master(tpl string) {
+	var err error
+	m.master, err = template.New(tpl).ParseFiles(filepath.Join(TemplateDir, tpl))
+	if err != nil {
+		log.Println("Unable to parse master template:", err)
+	}
+}
+
+func (m *templateManager) Get(tplFiles []string) (*template.Template, error) {
+	key := strings.Join(tplFiles, "|")
+
+	m.mu.RLock()
+	t, _ := m.templates[key]
+	m.mu.RUnlock()
+	if t == nil {
+		var (
+			err   error
+			files []string
+		)
+		for _, tplFile := range tplFiles {
+			files = append(files, filepath.Join(TemplateDir, tplFile))
+		}
+
+		t, err = template.Must(m.master.Clone()).ParseFiles(files...)
+		if err != nil {
+			return nil, errors.Wrapf(err, "unable to parse template %q", tplFiles)
+		}
+		m.mu.Lock()
+		m.templates[key] = t
+		m.mu.Unlock()
+	}
+
+	return t, nil
+}
+
+func Master(tpl string) {
+	mgr.Master(tpl)
+}
+
+func Render(ctx *fasthttp.RequestCtx, data map[string]interface{}, tpl ...string) {
+	RenderWithStatus(ctx, data, fasthttp.StatusOK, tpl...)
+}
+
+func RenderWithStatus(ctx *fasthttp.RequestCtx, data map[string]interface{}, status int, tpl ...string) {
+	t, err := mgr.Get(tpl)
+	if err != nil {
+		ctx.SetStatusCode(fasthttp.StatusInternalServerError)
+		ctx.SetContentType("text/plain")
+		ctx.WriteString("Unable to parse template: " + err.Error())
+		return
+	}
+
+	err = t.Execute(ctx, data)
+	if err != nil {
+		ctx.SetStatusCode(fasthttp.StatusInternalServerError)
+		ctx.SetContentType("text/plain")
+		ctx.WriteString("Unable to execute template: " + err.Error())
+		return
+	}
+
+	ctx.SetStatusCode(status)
+	ctx.SetContentType("text/html")
+}
